22ab94d9682558c5baac159afbc1d9538faf0a2b
nq17 <nquiles6@mail.valenciacollege.edu>
2026-04-30 21:59:07 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function videoFunction() {
+    document.getElementById("videoComment").innerHTML += " A great video about mental health awareness.";
+}
+
+function emailMessage() {
+    document.getElementById("emailLabel").innerHTML += " Please enter a valid email address";
+}
+
+function blankMessage() {
+    document.getElementById("emailLabel").innerHTML = "Email:";
+}
